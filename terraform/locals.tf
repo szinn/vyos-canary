c545fb07bb65bccafbf212fc14dd7847af56687b
Scotte Zinn <scotte@zinn.ca>
2023-07-15 09:20:24 -0400

    Update vyos starting image

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,9 +1,9 @@
 locals {
   pm_host_name = data.sops_file.secrets.data["proxmox_hostname"]
 
-  vyos_iso = "local:iso/vyos-1.4-rolling-202306041314-amd64.iso"
+  vyos_iso = "local:iso/vyos-1.4-rolling-202306251347-amd64.iso"
 
-  network_20_macaddr = "DE:CA:FF:20:00:F0"  
-  network_98_macaddr = "DE:CA:FF:98:01:F0"  
-  network_99_macaddr = "DE:CA:FF:99:02:F0"  
+  network_20_macaddr = "DE:CA:FF:20:00:F0"
+  network_98_macaddr = "DE:CA:FF:98:01:F0"
+  network_99_macaddr = "DE:CA:FF:99:02:F0"
 }
